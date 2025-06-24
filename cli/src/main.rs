f2cec175797b157b507e712226f33291e6953564
aacebo <aacebowork@gmail.com>
2025-06-24 19:08:55 +0100

    Update main.rs

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,12 @@
+use std::fs;
+
 fn main() {
     for path in std::env::args().skip(1) {
-        println!("path: {}", path);
+        let data = match fs::read(path) {
+            Ok(v) => v,
+            Err(err) => panic!("{}", err),
+        };
+
+        println!("{}", String::from_utf8(data).unwrap());
     }
 }
